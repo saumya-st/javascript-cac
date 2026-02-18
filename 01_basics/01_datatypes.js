24875affd76bf06c1fd179178a7f11bbe60ad390
saumya-st <saumyacodes02@gmail.com>
2026-02-18 18:41:14 +0530

    commit

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code according to modern version
+// use spaces and colons according to user readability
+// numbers lies in range of 2 to power of 23
+//big int
+//string => ""
+// bolean => true/false
+// null = > standalone value
+//symbol -> find uniqueness
+//object
+
+
+console.log(typeof "saumya")
+console.log(typeof  null) //object
+console.log(typeof undefined) //undefined
