5daefb60321760aca86177c97c683b228d87448c
saumya-st <saumyacodes02@gmail.com>
2026-02-18 20:42:43 +0530

    update

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = "33";
+console.log(typeof score)
+
+//conversion into number
+let newscore = Number(score)
+
+console.log(typeof newscore);
+
+
+//not convertible gives value as Nan but type is "Number" only
+
+
+// booolean conversion
+// 1 -> true
+// 0 -> false
+// empty string -> false
+// string -> true
+
+
+
+
+//  OPERATIONS
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+
+// PREFIX POSTFIX OPERATORS
---
+++ 01_basics/datatypess_summary.js
@@ -0,0 +1,10 @@
+//primitive  
+// 7 types  String number boolean null undefined symbol BigInt
+// DYNAMICALLLYYYY TYPEDDDD
+
+//reference typess(non primitive)
+// array objects function
+
+
+const heros = ["shaktiman", "naagraaj" ,"doga"];
+
